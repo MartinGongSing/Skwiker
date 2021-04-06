3a03edba0a772a0d0c4fc5375609a53a5b75e5cc
MartinGongSing <47624744+MartinGongSing@users.noreply.github.com>
2021-04-06 11:11:38 +0200

    demo

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,43 @@
+const version = 2
+
+self.addEventListener('install', event => {
+	event.waitUntil(
+		caches.open('static-v'+version) // use another cache
+		.then(cache => cache.addAll([
+			'/offline.html'
+		]))
+	)
+	self.skipWaiting() // control page immediately
+})
+
+const expectedCaches = [
+	'static-v'+version
+]
+
+self.addEventListener('activate', event => {
+	event.waitUntil(
+		// Cleanup old cache
+		caches.keys().then(cacheNames =>{
+			return Promise.all(
+				cacheNames.map(cacheName => {
+					if(!expectedCaches.includes(cacheName)) {
+						return caches.delete(cacheName)
+					}
+				})
+			)
+		})
+	)
+
+	self.clients.claim() // control all current clients (tabs)
+})
+
 self.addEventListener('fetch', event => {
-	console.log('Request:', event.request.url)
 	event.respondWith(
-		fetch(event.request).then(response => {
-			console.log('Response:', response.headers.get('Content-Type'))
-			return response
+		fetch(event.request)
+		.catch(() => {
+			if(event.request.mode == 'navigate') {
+				return caches.match('offline.html')
+			}
 		})
 	)
 })
